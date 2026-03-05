a5b724170749127f39f1258fb3a221ccbab19fa6
aakaaashmann <aks5060708090100@gmail.com>
2026-03-05 15:37:42 +0530

    create database , inserted 100 users dynamically,Create a separate Address collection with 10 city records and link it to Users using a reference ID

---
+++ main.py
@@ -0,0 +1,58 @@
+from fastapi import FastAPI
+from database import users_collection, address_collection
+from bson import ObjectId
+import random
+app = FastAPI()
+
+@app.get("/", tags=["Root"])
+def home():
+    return {"message": "FastAPI + MongoDB Working 🚀"}
+
+@app.post("/create-addresses", tags=["Create"])
+def create_addresses():
+    cities = ["Delhi", "Mumbai", "Bangalore", "Chennai", "Kolkata",
+              "Hyderabad", "Pune", "Allahabad", "Varanasi", "Lucknow"
+    ]
+
+    data = [{"city": city} for city in cities]
+    address_collection.insert_many(data)
+
+    return {"message": "Addresses inserted"}
+
+@app.post("/insert-users", tags=["Create"])
+def insert_users():
+    addresses = list(address_collection.find())
+
+    users = []
+
+    for i in range(100):
+        user = {
+            "name": f"User{i}",
+            "age": random.randint(15,75),
+            "address_id": random.choice(addresses)["_id"]
+        }
+        users.append(user)
+
+    users_collection.insert_many(users)
+
+    return {"message": "100 users inserted"}
+
+@app.get("/users-under-25", tags=["Find"])
+def users_under_25():
+    users = list(users_collection.find({"age": {"$lt": 25}}))
+
+    for user in users:
+        user["_id"] = str(user["_id"])
+        user["address_id"] = str(user["address_id"])
+
+    return users
+
+@app.get("/users-over-60", tags=["Find"])
+def users_over_60():
+    users = list(users_collection.find({"age": {"$gt": 60}}))
+
+    for user in users:
+        user["_id"] = str(user["_id"])
+        user["address_id"] = str(user["address_id"])
+
+    return users
